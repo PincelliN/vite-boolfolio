b8e886daefbf3c02a50f23e6b37521f6b39ef09a
Nicolò Pincelli <pincelli.n@gmail.com>
2024-10-02 22:09:48 +0200

    Add tecprojects and typeProjects

--- src/pages/TecProjects.vue
+++ src/pages/TecProjects.vue
@@ -1,11 +1,49 @@
 <script>
+import { store } from "../store/store";
+import axios from 'axios';
+import { getType, getLocalDate } from "../data/function";
+
 export default {
-    name: 'tecprojects'
+    name: 'tecprojects',
+    data() {
+        return {
+            techprojects: [],
+            techname: '',
+            getType,
+            getLocalDate
+
+        }
+    },
+    methods: {
+        getApi(slug) {
+            axios.get(store.apiUrl + 'Tutti-i progetti-per-Tecnologie/' + slug).then(res => {
+                console.log('1', res.data.result[0].name)
+                if (res.data.success) {
+                    this.techname = res.data.result[0].name
+                    this.techprojects = (res.data.result[0].works)
+                    console.log('2', this.techprojects);
+                } else {
+                    this.$router.push({ name: 'NotFound' })
+                }
+
+            })
+        }
+    },
+    mounted() {
+
+        const slug = this.$route.params.slug
+        this.getApi(slug)
+    }
 }
 </script>
-
 <template>
-    <h1>tecprojects</h1>
+    <h2>{{ techname }}</h2>
+    <ul>
+        <li v-for="techpro in techprojects"><router-link :to="{ name: 'dettaglio', params: { slug: techpro.slug } }">{{
+                techpro.title }}</router-link>
+            <br>
+            <small>Type: {{ getType(techpro) }} | Data:{{ getLocalDate(techpro.created_at) }}</small>
+        </li>
+    </ul>
 </template>
-
 <style lang="scss" scoped></style>
--- src/pages/TypeProjects.vue
+++ src/pages/TypeProjects.vue
@@ -20,7 +20,7 @@ export default {
                 if (res.data.success) {
                     this.typename = res.data.result[0].name
                     this.typeprojects = (res.data.result[0].works)
-                    console.log(this.typeprojects);
+                    console.log('qui', this.typeprojects[0]);
                 } else {
                     this.$router.push({ name: 'NotFound' })
                 }
@@ -38,8 +38,9 @@ export default {
 <template>
     <h2>{{ typename }}</h2>
     <ul>
-        <li v-for="typro in typeprojects">
-            {{ typro.title }}<br>
+        <li v-for="typro in typeprojects"><router-link :to="{ name: 'dettaglio', params: { slug: typro.slug } }">{{
+            typro.title }}</router-link><br>
+
             <small> Technologies:{{ getTechnologies(typro) }} |
                 Data:{{ getLocalDate(typro.created_at) }}</small>
         </li>
